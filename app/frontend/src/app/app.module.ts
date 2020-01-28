681fc86cf669bd778b471019a514fd2ad5ff173d
Nadrill <pawel.gluszczak1@gmail.com>
2020-01-28 20:43:20 +0100

    modules fixes

--- app/frontend/src/app/app.module.ts
+++ app/frontend/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, TRANSLATIONS, LOCALE_ID } from '@angular/core';
-
-import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
