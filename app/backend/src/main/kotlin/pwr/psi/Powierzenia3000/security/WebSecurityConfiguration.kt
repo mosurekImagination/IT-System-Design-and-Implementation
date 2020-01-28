7dba42ad2ce76228398726fc85aba68f682f0291
Nadrill <pawel.gluszczak1@gmail.com>
2020-01-28 23:21:40 +0100

    CORS policy

--- app/backend/src/main/kotlin/pwr/psi/Powierzenia3000/security/WebSecurityConfiguration.kt
+++ app/backend/src/main/kotlin/pwr/psi/Powierzenia3000/security/WebSecurityConfiguration.kt
@@ -1,9 +1,14 @@
 package pwr.psi.Powierzenia3000.security
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
+
 
 @EnableWebSecurity
 open class WebSecurityConfiguration()
@@ -11,10 +16,23 @@ open class WebSecurityConfiguration()
 
     override fun configure(http: HttpSecurity) {
         http.csrf().disable()
+        http.cors()
         http.authorizeRequests()
 //                .antMatchers("/api/*").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin().permitAll()
     }
+
+    @Bean
+    fun corsConfigurationSource() =
+            UrlBasedCorsConfigurationSource().also {
+                it.registerCorsConfiguration("/**",
+                        CorsConfiguration().apply {
+                            allowCredentials = true
+                            allowedHeaders = listOf("Authorization")
+                            allowedOrigins = listOf("*")
+                            allowedMethods = listOf("*")
+                        })
+            }
 }
