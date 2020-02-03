5632d6c89f4a309bbba17d362d2342ea0b800c1d
Nadrill <pawel.gluszczak1@gmail.com>
2020-02-03 20:09:14 +0100

    entrustment details put

--- app/frontend/src/app/profile/entrustment-details/dialog/dialog.component.ts
+++ app/frontend/src/app/profile/entrustment-details/dialog/dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from "@angular/core";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from "@angular/material";
 import { Router } from "@angular/router";
+import { ApiService } from "src/app/core/api.service";
+import { Entrustment } from "src/shared/models/Entrustment";
 
 @Component({
   selector: "app-dialog",
@@ -10,7 +12,10 @@ import { Router } from "@angular/router";
 export class DialogComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
-    private router: Router
+    private router: Router,
+    private apiService: ApiService,
+    private snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) {}
 
   ngOnInit() {}
@@ -20,7 +25,24 @@ export class DialogComponent implements OnInit {
   }
 
   onYesClick(): void {
+    this.apiService
+      .put<Entrustment>(`entrustment/update`, this.data.entrustment)
+      .subscribe(response => {
+        console.log("Put success");
+        this.openSnackbar("Wysłano!");
+      });
+
     this.router.navigate(["/profile"]);
     this.dialogRef.close();
   }
+
+  private openSnackbar(message: string) {
+    this.snackBar.open(message, "OK", {
+      duration: 4000
+    });
+  }
+}
+
+export interface DialogData {
+  entrustment: Entrustment;
 }
