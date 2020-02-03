8e563051be79d3bb7ba5d5db5d81978e7efbdc10
Nadrill <pawel.gluszczak1@gmail.com>
2020-02-03 19:25:17 +0100

    raports fixes

--- app/frontend/src/app/raport/current-entrustments/current-entrustments.component.ts
+++ app/frontend/src/app/raport/current-entrustments/current-entrustments.component.ts
@@ -5,6 +5,7 @@ import { ApiService } from "src/app/core/api.service";
 import { map, switchMap } from "rxjs/operators";
 import { CourseType } from "src/shared/models/Course";
 import { CourseTranslation } from "src/shared/models/CourseTranslation";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: "app-current-entrustments",
@@ -17,13 +18,15 @@ export class CurrentEntrustmentsComponent implements OnInit {
   ngOnInit() {
     // TODO : WHY THIS DOESNT WORK ?!
     this.apiService
-      .get<Entrustment[]>(`raport/free`)
+      .get<Entrustment[]>(`raport/all`)
       .pipe(
         map((entrustments: Entrustment[]) => {
           return entrustments.map(
             (ent: Entrustment) =>
               ({
-                courseName: "cokolwiek",
+                courseName: ent.courseId.translation.find(
+                  trl => trl.languageCode == environment.language
+                ).name,
                 form: CourseType[ent.courseId.courseType],
                 hours: ent.hours,
                 message: ent.answer,
